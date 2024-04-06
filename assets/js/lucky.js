da5a6bc71ea5c585a6f526048a1a7a563680e380
Vcaze <viktor.nordenberg1@gmail.com>
2024-04-06 04:26:34 +0200

    1.0.0 initial release

---
+++ assets/js/lucky.js
@@ -0,0 +1,64 @@
+console.log("lucky.js started");
+console.info("lucky.js started");
+
+const imgPath = "../../img/";
+
+const numberFilenames = [
+    "zero.png",
+    "one.png",
+    "two.png",
+    "three.png",
+    "four.png",
+    "five.png",
+    "six.png",
+    "seven.png",
+    "eight.png",
+    "nine.png",
+    "times.png",
+    "equals.png"
+];
+
+// Get the two factors and their product
+let product = 1000;
+let factorOne;
+let factorTwo;
+while (product > 999) {
+    // Randomize factor one and two
+    factorOne = Math.floor(Math.random() * 90) + 10;
+    factorTwo = Math.floor(Math.random() * 90) + 10;
+    // Get the product of the factors
+    product = factorOne * factorTwo;
+}
+
+// Factors and product to strings
+const factorOneString = factorOne.toString();
+const factorTwoString = factorTwo.toString();
+const productString = product.toString();
+
+// Add the numbers and signs to the divs on the page
+addImageToDiv(factorOneString[0], "factor-one-first");
+addImageToDiv(factorOneString[1], "factor-one-second");
+addImageToDiv(10, "times");
+addImageToDiv(factorTwoString[0], "factor-two-first");
+addImageToDiv(factorTwoString[1], "factor-two-second");
+addImageToDiv(11, "equals");
+addImageToDiv(productString[0], "product-first");
+addImageToDiv(productString[1], "product-second");
+addImageToDiv(productString[2], "product-third");
+
+// Function to add the image for a number or a sign to a div
+function addImageToDiv(number, divClassName) {
+    const div = document.getElementsByClassName(divClassName)[0];
+    let img = document.createElement("img");
+    img.src = imgPath + numberFilenames[parseInt(number)];
+    div.appendChild(img);
+    div.classList.add("zoom-in");
+}
+
+// debugging
+console.log("lucky.js is being run fully");
+
+console.log("fac 1: " + factorOne);
+console.log("fac 2: " + factorTwo);
+console.log("prod: " + product);
+console.log("imgpath: " + imgPath + "one");
